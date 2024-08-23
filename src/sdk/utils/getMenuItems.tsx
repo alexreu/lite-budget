2512907962f809f134de5f29fc421ba4b36b490c
Alexandre Adolphe <aadolphe@simplon.co>
2024-08-23 18:40:48 +0400

    WIP - menu

---
+++ src/sdk/utils/getMenuItems.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { MenuProps } from "antd";
+import {
+  BookMarked,
+  CreditCard,
+  HandCoins,
+  Home,
+  Pencil,
+  Tags,
+} from "lucide-react";
+
+type MenuItem = Required<MenuProps>["items"][number];
+
+export const MenuItems: MenuItem[] = [
+  {
+    label: "Accueil",
+    icon: <Home size={14} />,
+    key: "home",
+  },
+  {
+    label: "Budgets",
+    icon: <HandCoins size={14} />,
+    key: "budgets",
+  },
+  {
+    label: "Comptes",
+    icon: <BookMarked size={14} />,
+    key: "accounts",
+  },
+  {
+    label: "Transactions",
+    icon: <CreditCard size={14} />,
+    key: "transactions",
+  },
+  {
+    label: "Catégories",
+    icon: <Pencil size={14} />,
+    key: "categories",
+  },
+  {
+    label: "Tags",
+    icon: <Tags size={14} />,
+    key: "tags",
+  },
+];
